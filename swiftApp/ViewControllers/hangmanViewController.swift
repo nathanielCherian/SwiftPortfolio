82f078cc08cf58a10bb6c413271de907cae5890a
Nathaniel Cherian <ngcherian2014@gmail.com>
2020-03-25 23:09:50 -0700

    fixed (inc)restart hangman

--- swiftApp/ViewControllers/hangmanViewController.swift
+++ swiftApp/ViewControllers/hangmanViewController.swift
@@ -19,19 +19,39 @@ class hangmanViewController: UIViewController {
     @IBOutlet weak var wordLabel: UILabel!
     @IBOutlet weak var usedLettersLabel: UILabel!
     @IBOutlet weak var inputField: UITextField!
+    @IBOutlet var restartButton: UIButton!
     
     
     var incorrectGuess = Int(-1)
-    var words : [String] = ["hello", "computer", "door", "hangman"]
+    var words : [String] = []
     var rand = Int(0) //temperary will make random in fuction start
     var myWord : [String] = [] // this array will be editable and displayed
     var wordRand : [Character] = []
     var guesNum = Int(0) // num of guesses passes to controller
+    var badLetters: [String] = []
 
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        restartButton.isHidden = true
+        
+        //usedLettersLabel.text = "h" + "\n" + "j"
+        let fileURLProject = Bundle.main.path(forResource: "wordlist", ofType: "txt")
+        var readStringProject = ""
+        do{
+            readStringProject = try String(contentsOfFile: fileURLProject!, encoding: String.Encoding.utf8)
+        }catch let error as NSError{
+            print("Failed to read")
+            print(error)
+        }
+        
+        let stringProjArr = readStringProject.components(separatedBy: "\n")
+        words = stringProjArr
+        
+        print(stringProjArr[2837])
+
+        //print(readStringProject)
         
         restart()
         start()
@@ -69,15 +89,24 @@ class hangmanViewController: UIViewController {
         leftLeg.isHidden = true
         rightLeg.isHidden = true
         wordLabel.text = ""
+        usedLettersLabel.text = ""
+        
+        
+        incorrectGuess = Int(-1)
+        rand = Int(0) //temperary will make random in fuction start
+        myWord = [] // this array will be editable and displayed
+        guesNum = Int(0) // num of guesses passes to controller
+        badLetters = []
     }
     
     
     func start() -> Void {
         guesNum = 0//reset to 0
-        rand = 3 // initialize rand
+        rand = Int.random(in: 0...4095) // initialize rand
         myWord = []
         wordRand = []
         wordRand = Array(words[rand])
+        print(words[rand])
         
         for character in wordRand{
             myWord.append("_ ")
@@ -103,8 +132,19 @@ class hangmanViewController: UIViewController {
             str = str + string
         }
         
+        
+        var usedStr: String = ""
+        for string in badLetters{
+            usedStr = usedStr + string
+        }
+        
+        usedLettersLabel.text = usedStr
         wordLabel.text = str
         inputField.text = ""
+        
+        if str.contains("_") == false{
+            restartButton.isHidden = false
+        }
     }
     
 
@@ -126,8 +166,11 @@ class hangmanViewController: UIViewController {
                         if controller(val: guesNum) == false{
                             restart()
                             start()
+                            break;
                         }
                         guesNum+=1
+                        badLetters.append(guess + "\n")
+
                     }
                 }
             }
@@ -135,8 +178,18 @@ class hangmanViewController: UIViewController {
         }else{
             
             
-            
         }
+    }
+    
+    
+    
+    @IBAction func restartButto(_ sender: Any) {
+        
+        restart()
+        start()
+        
+        restartButton.isHidden = true
+        
     }
     
     
